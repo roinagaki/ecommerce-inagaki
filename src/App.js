9ea68d14c125c9e01af7cfbe3897f31beb9f41ad
roinagaki <roinagaki@jotafi.com.ar>
2022-04-28 15:44:03 -0300

    Agregando NavBar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar'
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Rodrigo
-        </a>
-      </header>
+      <NavBar/>
     </div>
   );
 }
